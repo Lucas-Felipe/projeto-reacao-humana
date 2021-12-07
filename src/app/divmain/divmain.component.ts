2cfc11afc2a7d2a7be6c61dc3d8591c327422eec
lucas.felipe <l17.lucas@gmail.com>
2021-12-07 11:19:38 -0300

    modelo inicial

--- src/app/divmain/divmain.component.ts
+++ src/app/divmain/divmain.component.ts
@@ -6,10 +6,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./divmain.component.scss']
 })
 export class DivmainComponent implements OnInit {
+  aux?:number;
+  tecla?:number;
+  tempoinicial?:number;
+  tempofinal?:number;
+  acertos?:number;
+  tempototal?:number;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.acertos=0;
+    this.tempototal=0;
   }
 
+  Desenhabola(){
+    //this.aux= random;
+
+    if(){
+
+    }else if(){
+
+    }else if(){
+
+    }else if(){
+
+    }
+
+    const x=;
+    const y=;
+
+    Boolean.setattribute();
+
+    document.body.appendChild();
+    this.tempoinicial=Date.now();
+  }
+
+  inicioDeJogo(){
+    this.Desenhabola();
+  }
+
+  eventoDeTecla(event){
+
+  }
+
+  imprimeTempo(){
+
+  }
+
+  AtualizaTempo(){
+
+  }
+
+  RecarregaBola(){
+
+  }
 }
