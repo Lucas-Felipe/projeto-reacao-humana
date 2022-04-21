2fcc2b90f6258206a39c7e1c3051756da97784a8
Lucas Felipe Galvão Silva <l17.lucas@gmail.com>
2022-04-21 16:19:00 -0300

    colocando rounds, contagem funciona, aviso de fim de jogo funciona, resultados travados ao final de jogo não funciona, tempo de espera não implementado, indicação de cores não implementado.

--- src/app/divmain/divmain.component.ts
+++ src/app/divmain/divmain.component.ts
@@ -14,94 +14,112 @@ export class DivmainComponent implements OnInit {
   tempototal?:number;
   erros?:number;
   score?:number;
+  rounds?:number;
 
   @HostListener('document:keydown', ['$event'])
   handleKeyboardEvent(event: KeyboardEvent): void {
-    this.tempofinal=Date.now();
-    this.tecla=event.keyCode;
+    if (this.rounds!>0) {
+      this.tempofinal=Date.now();
+      this.tecla=event.keyCode;
 
-    if (this.tecla==this.aux){
-      this.acertos=this.acertos!+1;
-    }else{
-      this.erros=this.erros!+1;
+      if (this.tecla==this.aux){
+        this.acertos=this.acertos!+1;
+      }else{
+        this.erros=this.erros!+1;
+      }
+      this.imprimeTempo();
+      this.Desenhabola();
+
+    }else if(this.rounds!<10){
+
     }
-    this.imprimeTempo();
-    this.Desenhabola();
   }
 
   constructor() { }
 
   ngOnInit(): void {
-    this.acertos=0;
-    this.tempototal=0;
-    this.aux=0;
-    this.tecla=0;
-    this.tempoinicial=0;
-    this.tempofinal=0;
-    this.erros=0;
-    this.score=0;
+    this.Resetarvariaveis();
   }
 
   Desenhabola(){
-    this.aux= Math.floor(Math.random()*4);
-    const x=Math.floor(Math.random()*500);
-    const y=Math.floor(Math.random()*400);
-    const circulo = document.createElement('div');
-    if(this.aux==0){
-
-      circulo.setAttribute("id", "circulo"+this.aux);
-      circulo.setAttribute("class","circuloamarelo");
-      circulo.style.marginLeft = `${x}px`;
-      circulo.style.marginTop = `${y}px`;
-      circulo.style.backgroundColor = "yellow";
-      circulo.style.width = "60px";
-      circulo.style.height= "60px";
-      circulo.style.borderRadius= "30px";
-      this.aux=37;
-    }else if(this.aux==1){
-
-      circulo.setAttribute("id", "circulo"+this.aux);
-      circulo.setAttribute("class","circuloazul");
-      circulo.style.marginLeft = `${x}px`;
-      circulo.style.marginTop = `${y}px`;
-      circulo.style.backgroundColor = "blue";
-      circulo.style.width = "60px";
-      circulo.style.height= "60px";
-      circulo.style.borderRadius= "30px";
-      this.aux=38;
-    }else if(this.aux==2){
-
-      circulo.setAttribute("id", "circulo"+this.aux);
-      circulo.setAttribute("class","circuloverde");
-      circulo.style.marginLeft = `${x}px`;
-      circulo.style.marginTop = `${y}px`;
-      circulo.style.backgroundColor = "green";
-      circulo.style.width = "60px";
-      circulo.style.height= "60px";
-      circulo.style.borderRadius= "30px";
-      this.aux=39;
-    }else if(this.aux==3){
-
-      circulo.setAttribute("id", "circulo"+this.aux);
-      circulo.setAttribute("class","circulovermelho");
-      circulo.style.marginLeft = `${x}px`;
-      circulo.style.marginTop = `${y}px`;
-      circulo.style.backgroundColor = "red";
-      circulo.style.width = "60px";
-      circulo.style.height= "60px";
-      circulo.style.borderRadius= "30px";
-      this.aux=40;
-    }
-
     const documento = document.getElementById("divjogo");
-    if (this.acertos!=0 || this.erros!=0) {
-      documento?.replaceChild(circulo, documento.children[0]);
+    if (this.rounds!>=10) {
+      const fimDeJogo = document.createElement('div');
+      fimDeJogo.innerText= "Fim de Jogo! Seus resultados estão ao lado! "+
+      "\nPara jogar novamente, clique em 'Reiniciar jogo'";
+      fimDeJogo.style.width="50%";
+      fimDeJogo.style.marginLeft="25%";
+      fimDeJogo.style.marginTop="35%";
+      fimDeJogo.style.color="red";
+      fimDeJogo.style.textAlign="center";
+      fimDeJogo.style.borderWidth="1%";
+      fimDeJogo.style.borderColor="black";
+      fimDeJogo.style.borderStyle="solid";
+
+      documento?.replaceChild(fimDeJogo, documento.children[0]);
     }else{
-      documento?.appendChild(circulo);
+      this.aux= Math.floor(Math.random()*4);
+      const x=Math.floor(Math.random()*500);
+      const y=Math.floor(Math.random()*400);
+      const circulo = document.createElement('div');
+      if(this.aux==0){
+
+        circulo.setAttribute("id", "circulo"+this.aux);
+        circulo.setAttribute("class","circuloamarelo");
+        circulo.style.marginLeft = `${x}px`;
+        circulo.style.marginTop = `${y}px`;
+        circulo.style.backgroundColor = "yellow";
+        circulo.style.width = "60px";
+        circulo.style.height= "60px";
+        circulo.style.borderRadius= "30px";
+        this.aux=37;
+      }else if(this.aux==1){
+
+        circulo.setAttribute("id", "circulo"+this.aux);
+        circulo.setAttribute("class","circuloazul");
+        circulo.style.marginLeft = `${x}px`;
+        circulo.style.marginTop = `${y}px`;
+        circulo.style.backgroundColor = "blue";
+        circulo.style.width = "60px";
+        circulo.style.height= "60px";
+        circulo.style.borderRadius= "30px";
+        this.aux=38;
+      }else if(this.aux==2){
+
+        circulo.setAttribute("id", "circulo"+this.aux);
+        circulo.setAttribute("class","circuloverde");
+        circulo.style.marginLeft = `${x}px`;
+        circulo.style.marginTop = `${y}px`;
+        circulo.style.backgroundColor = "green";
+        circulo.style.width = "60px";
+        circulo.style.height= "60px";
+        circulo.style.borderRadius= "30px";
+        this.aux=39;
+      }else if(this.aux==3){
+
+        circulo.setAttribute("id", "circulo"+this.aux);
+        circulo.setAttribute("class","circulovermelho");
+        circulo.style.marginLeft = `${x}px`;
+        circulo.style.marginTop = `${y}px`;
+        circulo.style.backgroundColor = "red";
+        circulo.style.width = "60px";
+        circulo.style.height= "60px";
+        circulo.style.borderRadius= "30px";
+        this.aux=40;
+      }
+
+
+      if (this.acertos!=0 || this.erros!=0) {
+        documento?.replaceChild(circulo, documento.children[0]);
+      }else{
+        documento?.appendChild(circulo);
+      }
+
+
+      this.rounds!+=1;
+
+      this.tempoinicial=Date.now();
     }
-
-    this.tempoinicial=Date.now();
-
   }
 
   inicioDeJogo(){
@@ -120,12 +138,21 @@ export class DivmainComponent implements OnInit {
     document.getElementById("score")!.innerHTML="Score: "+((this.acertos!-this.erros!)as unknown)as string;
   }
 
-  // AtualizaTempo(){
+  Resetarvariaveis(): void{
 
-  //   this.imprimeTempo(tempo);
-  // }
+    this.acertos=0;
+    this.tempototal=0;
+    this.aux=0;
+    this.tecla=0;
+    this.tempoinicial=0;
+    this.tempofinal=0;
+    this.erros=0;
+    this.score=0;
+    this.rounds=0;
+  }
 
-  // RecarregaBola(){
-  //   document.location.reload();
-  // }
+  ResetarJogo():void{
+    this.Resetarvariaveis();
+    window.location.reload();
+  }
 }
