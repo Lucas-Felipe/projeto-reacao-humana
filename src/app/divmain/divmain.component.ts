986e7e43fa6a0a1595dce8566670e285cd503126
Lucas Felipe Galvão Silva <l17.lucas@gmail.com>
2022-04-21 15:06:32 -0300

    jogo funcionando. Faltando: resetar jogo, fazer jogo contar rounds e ter limite até 10 rounds, fazer jogo passar pro próximo round caso demore demais.

--- src/app/divmain/divmain.component.ts
+++ src/app/divmain/divmain.component.ts
@@ -1,6 +1,5 @@
-import { createOfflineCompileUrlResolver } from '@angular/compiler';
+
 import { Component, HostListener, OnInit } from '@angular/core';
-import {ViewChild, ElementRef} from '@angular/core';
 @Component({
   selector: 'app-divmain',
   templateUrl: './divmain.component.html',
@@ -13,17 +12,21 @@ export class DivmainComponent implements OnInit {
   tempofinal?:number;
   acertos?:number;
   tempototal?:number;
+  erros?:number;
+  score?:number;
 
   @HostListener('document:keydown', ['$event'])
   handleKeyboardEvent(event: KeyboardEvent): void {
+    this.tempofinal=Date.now();
     this.tecla=event.keyCode;
-    if (this.tecla==this.aux) {
-      this.acertos=this.acertos!+1;
-      this.tempofinal=Date.now();
-      this.AtualizaTempo();
-    }else
-      this.RecarregaBola();
 
+    if (this.tecla==this.aux){
+      this.acertos=this.acertos!+1;
+    }else{
+      this.erros=this.erros!+1;
+    }
+    this.imprimeTempo();
+    this.Desenhabola();
   }
 
   constructor() { }
@@ -35,6 +38,8 @@ export class DivmainComponent implements OnInit {
     this.tecla=0;
     this.tempoinicial=0;
     this.tempofinal=0;
+    this.erros=0;
+    this.score=0;
   }
 
   Desenhabola(){
@@ -46,9 +51,8 @@ export class DivmainComponent implements OnInit {
 
       circulo.setAttribute("id", "circulo"+this.aux);
       circulo.setAttribute("class","circuloamarelo");
-      circulo.style.left = `${x}px`;
-      circulo.style.top = `${y}px`;
-      circulo.style.margin = "100px";
+      circulo.style.marginLeft = `${x}px`;
+      circulo.style.marginTop = `${y}px`;
       circulo.style.backgroundColor = "yellow";
       circulo.style.width = "60px";
       circulo.style.height= "60px";
@@ -57,11 +61,10 @@ export class DivmainComponent implements OnInit {
     }else if(this.aux==1){
 
       circulo.setAttribute("id", "circulo"+this.aux);
-      circulo.setAttribute("class","circulolaranja");
-      circulo.style.left = `${x}px`;
-      circulo.style.top = `${y}px`;
-      circulo.style.margin = "100px";
-      circulo.style.backgroundColor = "orange";
+      circulo.setAttribute("class","circuloazul");
+      circulo.style.marginLeft = `${x}px`;
+      circulo.style.marginTop = `${y}px`;
+      circulo.style.backgroundColor = "blue";
       circulo.style.width = "60px";
       circulo.style.height= "60px";
       circulo.style.borderRadius= "30px";
@@ -70,9 +73,8 @@ export class DivmainComponent implements OnInit {
 
       circulo.setAttribute("id", "circulo"+this.aux);
       circulo.setAttribute("class","circuloverde");
-      circulo.style.left = `${x}px`;
-      circulo.style.top = `${y}px`;
-      circulo.style.margin = "100px";
+      circulo.style.marginLeft = `${x}px`;
+      circulo.style.marginTop = `${y}px`;
       circulo.style.backgroundColor = "green";
       circulo.style.width = "60px";
       circulo.style.height= "60px";
@@ -82,9 +84,8 @@ export class DivmainComponent implements OnInit {
 
       circulo.setAttribute("id", "circulo"+this.aux);
       circulo.setAttribute("class","circulovermelho");
-      circulo.style.left = `${x}px`;
-      circulo.style.top = `${y}px`;
-      circulo.style.margin = "100px";
+      circulo.style.marginLeft = `${x}px`;
+      circulo.style.marginTop = `${y}px`;
       circulo.style.backgroundColor = "red";
       circulo.style.width = "60px";
       circulo.style.height= "60px";
@@ -93,31 +94,38 @@ export class DivmainComponent implements OnInit {
     }
 
     const documento = document.getElementById("divjogo");
-    documento?.appendChild(circulo);
-    document.getElementById("divjogo")?.appendChild(circulo);
+    if (this.acertos!=0 || this.erros!=0) {
+      documento?.replaceChild(circulo, documento.children[0]);
+    }else{
+      documento?.appendChild(circulo);
+    }
 
     this.tempoinicial=Date.now();
 
   }
 
   inicioDeJogo(){
-
     this.Desenhabola();
   }
 
-  imprimeTempo(tempo:any){
-    this.tempototal=this.tempototal+tempo;
-    document.getElementById("temporeacao")!.innerHTML="Tempo de Reação: "+tempo;
-    document.getElementById("tempomedio")!.innerHTML="Tempo médio: "+((this.tempototal!/this.acertos!)as unknown)as string;
-  }
-
-  AtualizaTempo(){
-    var tempo=(this.tempofinal!-this.tempoinicial!)/1000;
+  imprimeTempo():void{
+    const tempoDeReacao=(this.tempofinal!-this.tempoinicial!)/1000;
+    document.getElementById("temporeacao")!.innerHTML="Tempo de Reação: "+tempoDeReacao;
+    this.tempototal=this.tempototal!+tempoDeReacao;
+    this.acertos!=0 ?
+      document.getElementById("tempomedio")!.innerHTML="Tempo médio: "+((this.tempototal!/this.acertos!)as unknown)as string :
+      document.getElementById("tempomedio")!.innerHTML="Tempo médio: "+ 0;
     document.getElementById("acertos")!.innerHTML="Acertos: "+(this.acertos! as unknown)as string;
-    this.imprimeTempo(tempo);
+    document.getElementById("erros")!.innerHTML="Erros: "+(this.erros! as unknown)as string;
+    document.getElementById("score")!.innerHTML="Score: "+((this.acertos!-this.erros!)as unknown)as string;
   }
 
-  RecarregaBola(){
-    document.location.reload();
-  }
+  // AtualizaTempo(){
+
+  //   this.imprimeTempo(tempo);
+  // }
+
+  // RecarregaBola(){
+  //   document.location.reload();
+  // }
 }
